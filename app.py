ff536f3fe407259aaded66707c2fcdc7a61ad8ea
Fábio Ngila <fabiongila@gmail.com>
2026-02-14 18:04:00 +0100

    hospedagem no Render

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from models import db
 from routes import api_routes
+import os
 
 
 app = Flask(__name__)
@@ -20,4 +21,5 @@ app.register_blueprint(api_routes)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+   port= int(os.environ.get("PORT", 5000))
+   app.run(host="0.0.0.0", port=port)

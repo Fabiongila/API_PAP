6d95b8e75fba073eecbcbbeb883aefc28a8316f7
Fábio Ngila <fabiongila@gmail.com>
2026-02-14 20:02:24 +0100

    corre3

--- app.py
+++ app.py
@@ -21,4 +21,5 @@ app.register_blueprint(api_routes)
 
 
 if __name__ == '__main__':
-   app.run(host="0.0.0.0", port= int(os.environ.get("PORT", 5000)))
+   port= int(os.environ.get("PORT", 5000))
+   app.run(host="0.0.0.0", port=port)
